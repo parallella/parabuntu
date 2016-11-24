f030b60268b80b8720fc545b01ac3723690de3ea
Ola Jeppsson <ola@adapteva.com>
2016-11-24 10:20:37 +0100

    Use 2016.11 branches in tests and scripts
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- tests/epiphany-sdk/test.sh
+++ tests/epiphany-sdk/test.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf epiphany-examples &&
-git clone https://github.com/adapteva/epiphany-examples.git --branch 2015.1 &&
+git clone https://github.com/adapteva/epiphany-examples.git --branch 2016.11 &&
 cd epiphany-examples &&
 ./scripts/build_and_test_all.sh &&
 echo Verify results manually &&
